23faad3f360325ae14fb4d024a31d6149940af6c
royws <roywildeboerschut@gmail.com>
2017-05-16 14:53:34 +0200

    first commit

---
+++ Assets/Scripts/attachToPlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attachToPlatform : MonoBehaviour {
+
+    //public GameObject attachedObject;
+
+	// Use this for initialization
+	void Start () {
+
+        print("attach script active");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = transform.localScale;
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "platform")
+        {
+            print("attach");
+            //attachedObject = col.transform.parent.gameObject;
+            transform.parent = col.transform;
+        }
+    }
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "platform")
+        {
+            print("attach");
+            transform.parent = null;
+        }
+    }
+}
---
+++ Assets/Scripts/cubeMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeMove : MonoBehaviour {
+
+    private bool dirRight = true;
+    public float speed = 2f;
+    //public GameObject parent;
+
+    // Use this for initialization
+    void Start () {
+        //gameObject.transform.parent = parent.transform;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (dirRight)
+        {
+            //parent.transform.Translate(Vector3.right * (speed * 3) * Time.deltaTime);
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            //parent.transform.Translate(Vector3.left * (speed * 3) * Time.deltaTime);
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (transform.position.x >= 7.0f)
+        {
+            dirRight = false;
+        }
+
+        if (transform.position.x <= 4)
+        {
+            dirRight = true;
+        }
+    }
+}
